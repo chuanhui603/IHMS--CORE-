9e9eea66d338322bd7b51e252ebb4c2b950f7aa1
Hui <chuanhui603@gmail.com>
2023-06-18 18:52:54 +0800

    更新

---
+++ IHMS/Models/AnnouncementView.cs
@@ -0,0 +1,11 @@
+﻿namespace IHMS.Models
+{
+    public class AnnouncementView
+    {
+        public int an_announcemet_id { get; set; }
+        public string an_title { get; set; }
+        public string an_content { get; set; }
+        public DateTime an_time { get; set; }
+        public string an_image { get; set; }
+    }
+}
