10a56254756743db8612ece010c1c4be8dad150e
Hui <chuanhui603@gmail.com>
2023-06-18 23:04:09 +0800

    更新

---
+++ IHMS/Models/Questionset.cs
@@ -0,0 +1,10 @@
+﻿namespace IHMS.Models
+{
+    public class Questionset
+    {
+        public int q_questionset_id { get; set; }
+        public string q_question { get; set; }
+        public string q_category { get; set; }
+        public string q_api_url { get; set; }
+    }
+}
