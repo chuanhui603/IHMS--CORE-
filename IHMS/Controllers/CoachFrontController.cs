fc31b77f47ea312d11efd345a5a4b288e16b35f5
SiennaYang <siennayang.yty@gmail.com>
2023-07-25 14:35:45 +0800

    新增coach list

---
+++ IHMS/Controllers/CoachFrontController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using IHMS.Models;
+using IHMS.ViewModel;
+using IHMS.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace IHMS.Controllers
+{
+    public class CoachFrontController : Controller
+    {
+        private IhmsContext db;
+        private readonly IhmsContext _context;
+        public CoachFrontController(IhmsContext context)
+        {
+            db = context;
+            _context = context;
+        }
+        //教練列表
+        public IActionResult CoachList(CKeywordViewModel v)
+        {
+            IEnumerable<Coach> datas = null;
+            if (!String.IsNullOrEmpty(v.txtKeyword))
+            {
+                string keyword = v.txtKeyword.ToLower();
+                datas = _context.Coaches
+                    .Include(c => c.Member)
+                    .Include(c => c.Courses)
+                    
+                    .Include(c => c.Intro)
+                    .Include(c => c.Type)
+                    .Include(c => c.Video)
+                    .Where(c => 
+                    (c.Member.Name.ToLower().Contains(keyword) || c.Member.ResidentialCity.ToLower().Contains(keyword)  ));
+            }
+            else
+            {
+                datas = _context.Coaches
+                                    .Include(c => c.Member)
+                    .Include(c => c.Courses)
+                    
+                    .Include(c => c.Intro)
+                    .Include(c => c.Type)
+                    .Include(c => c.Video)
+                                    .Where(c => c.Condition== 1);
+            }
+            ViewBag.Keyword = v.txtKeyword;
+            var coaches = CCoachViewModel.CoachList(datas.ToList());
+            return View(coaches);
+        }
+
+        //教練詳細資料
+        public IActionResult ViewCoachDetails(int? id)
+        {
+            IEnumerable<Coach> datas = null;
+            Coach coach = _context.Coaches.FirstOrDefault(c => c.CoachId == id);
+            if (coach == null)
+                return RedirectToAction("CoachList");
+
+            var data = _context.Coaches.Where(c => c.CoachId == id)
+                .Include(c => c.Image)
+                .Include(c => c.Courses)
+                .Include(c => c.Intro)
+                .Include(c => c.Type)
+                .Include(c => c.Video);
+
+            CCoachViewModel vModel = new CCoachViewModel
+            {
+                Coach = (Coach)data
+            };
+            return View(vModel);
+        }
+        
+
+        //教練預約時間表-可預約
+        public IActionResult getAvailableTimeId(int? id)
+        {
+            var ids = db.Schedules.Where(ca => ca.CourseId == id).Select(ca => ca.StartTime).Distinct();
+            return Json(ids);
+        }
+        
+        //通知教練
+        public IActionResult createContact(Member Email)
+        {
+            //取得登入者ID
+            
+            return Content("");
+        }
+
+
+        
+
+    }
+}
---
+++ IHMS/ViewModel/CCoachViewModel.cs
@@ -0,0 +1,112 @@
+﻿using IHMS.Models;
+
+namespace IHMS.ViewModel
+{
+    public class CCoachViewModel
+    {
+        public Coach Coach;
+        public Member Member;
+        public Course Course;
+        public CCoachViewModel()
+        {
+            Coach = new Coach();
+        }
+        
+        public CCoachViewModel(Coach c)
+        {
+            Coach = c;
+        }
+        static public List<CCoachViewModel> CoachList(List<Coach> coachList)
+        {
+            List<CCoachViewModel> list = new List<CCoachViewModel>();
+            foreach (var c in coachList)
+            {
+                CCoachViewModel vModel = new CCoachViewModel(c);
+                list.Add(vModel);
+            }
+            return list;
+        }
+        
+        public int CoachId
+        {
+            get { return Coach.CoachId; }
+        }
+        public int? MemberId
+        {
+            get { return Coach.MemberId; }
+            set { Coach.MemberId = (int)value; }
+        }
+        public string Name
+        {
+            get { return Member.Name; }
+            set { Member.Name = value; }
+        }
+        public bool? Gender
+        {
+            get { return Member.Gender; }
+            set { Member.Gender = value; }
+        }
+        public string City
+        {
+            get { return Member.ResidentialCity; }
+            set { Member.ResidentialCity = value; }
+        }
+        public string Intro
+        {
+            get { return Coach.Intro; }
+            set { Coach.Intro = value; }
+        }
+        public string Image
+        {
+            get { return Coach.Image; }
+            set { Coach.Image = value; }
+        }
+        
+        public string Type
+        {
+            get { return Coach.Type; }
+            set { Coach.Type = value; }
+        }
+        public double? Commission
+        {
+            get { return Coach.Commission; }
+            set { Coach.Commission = value; }
+        }
+        public DateTime ApplyDate
+        {
+            get { return Coach.Applytime; }
+            set { Coach.Applytime = value; }
+        }
+        public DateTime ConfirmDate
+        {
+            get { return Coach.Confirmtime; }
+            set { Coach.Confirmtime = value; }
+        }
+        public int? Rank
+        {
+            get { return Coach.Rank; }
+            set { Coach.Rank = value; }
+        }
+        public string Resume
+        {
+            get { return Coach.Resume; }
+            set { Coach.Resume = value; }
+        }
+        public string Video
+        {
+            get { return Coach.Video; }
+            set { Coach.Video = value; }
+        }
+        public int Condition
+        {
+            get { return Coach.Condition; }
+            set { Coach.Condition= value; }
+        }
+        public string Reason
+        {
+            get { return Coach.Reason; }
+            set { Coach.Reason = value; }
+        }
+        
+    }
+}
