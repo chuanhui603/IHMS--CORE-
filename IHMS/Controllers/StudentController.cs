2d69fd44442e41f6289a004241275ea147d04652
SiennaYang <siennayang.yty@gmail.com>
2023-07-26 16:42:11 +0800

    教練課程頁面

---
+++ IHMS/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IHMS.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
