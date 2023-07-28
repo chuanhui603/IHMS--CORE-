46fb978cd6c39f0d1cd5498327ccf44a0cf69b71
SiennaYang <siennayang.yty@gmail.com>
2023-07-29 03:20:12 +0800

    JOIN MEMBER TABLE

--- IHMS/Controllers/CoachController.cs
+++ IHMS/Controllers/CoachController.cs
@@ -32,9 +32,7 @@ namespace IHMS.Controllers
             IhmsContext db = new IhmsContext();
             //foreach (var c in db.Coaches.OrderByDescending(c => c.Applytime))
             //{
-               var datas = from d in db.Coaches
-                            where d.Condition == 1|| d.Condition == 0|| d.Condition == 2
-                           select d;
+               var datas = db.Coaches.Include("Member").Select(c => c.Condition == 1 || c.Condition == 0 || c.Condition == 2);
             //}
             return View(datas);
             //List<CCoachResumeViewModel> coaches = new List<CCoachResumeViewModel>();
