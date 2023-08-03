786ef5602d8762c0c0e8929ca145b99fe0088d8d
sherry1 <sherry224322@gmail.com>
2023-08-03 15:20:32 +0800

    更新時間跟點數list

--- IHMS/Models/PointRecord.cs
+++ IHMS/Models/PointRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IHMS.Models;
 
@@ -7,12 +8,16 @@ public partial class PointRecord
 {
     public int PointrecordId { get; set; }
 
+    [Display(Name = "數量")]
     public int Count { get; set; }
 
+    [Display(Name = "會員ID")]
     public int MemberId { get; set; }
 
+    [Display(Name = "銀行亂碼")]
     public int BankNumber { get; set; }
 
+    [Display(Name = "下單時間")]
     public DateTime? Createtime { get; set; }
 
     public DateTime? Updatetime { get; set; }
